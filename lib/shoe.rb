724a20c9c77e7549fa7e2357c33f67d3f3315f26
Justine Bricknell <justinegayton@gmail.com>
2018-07-02 20:59:22 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,17 @@
 class Shoe
+  attr_accessor :size, :color, :material
+  attr_reader :condition
 
-def initialized(brand)
-  @brand = brand
-end
+  def initialized(brand)
+    @brand = brand
+  end
 
-def brand 
-  @brand
-end
+  def brand
+    @brand
+  end
+
+  def cobble
+    :condition = new 
+  end
 
 end
